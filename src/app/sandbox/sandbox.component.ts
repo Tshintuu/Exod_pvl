54a120df3579c111043364ed0883c732bb39cf1b
Victor Todeschini <victodeschini@yahoo.fr>
2019-04-10 10:22:10 +0200

    Added a sandox component for tests

---
+++ src/app/sandbox/sandbox.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TweenLite } from 'gsap';
+
+@Component({
+  selector: 'app-sandbox',
+  templateUrl: './sandbox.component.html',
+  styleUrls: ['./sandbox.component.css']
+})
+export class SandboxComponent implements OnInit {
+
+  @ViewChild('light')
+
+  private _light: ElementRef;
+
+  private get light(): HTMLElement {
+    return this._light.nativeElement;
+  }
+
+  public activate(): TweenLite {
+    return TweenLite.fromTo(this.light, 0.5, { className: ''}, { className: 'active'});
+  }
+
+  public inactivate(): TweenLite {
+    return TweenLite.fromTo(this.light, 0.5, { className: 'active'}, { className: ''});
+  }
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
